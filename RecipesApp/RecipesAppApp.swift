2f73a2c527a7edb927f782eb46084c2241dfecb9
Massil1099 <massilaitchallal2002@gmail.com>
2025-11-17 10:00:41 +0100

    Initial commit - RecipesApp and Xcode project setup

---
+++ RecipesApp/RecipesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesAppApp.swift
+//  RecipesApp
+//
+//  Created by Massil ait challal on 17/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct RecipesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
