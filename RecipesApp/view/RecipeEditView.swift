8efff93904bbdd127d05cb97ee5c864006ab6fa3
Massil1099 <massilaitchallal2002@gmail.com>
2025-11-24 10:00:44 +0100

    RecipeEditView: Displaying the Ingredients in TextFields and adding the sortByName function

--- RecipesApp/view/RecipeEditView.swift
+++ RecipesApp/view/RecipeEditView.swift
@@ -8,46 +8,76 @@
 import Foundation
 import SwiftUI
 
+import SwiftUI
+
 struct RecipeEditView: View {
     
     @Binding var recipe: Recipe
     @State private var sortByName = false
-
+    
+    /// Retourne les indices triés selon le toggle
+    var sortedIndices: [Int] {
+        if sortByName {
+            return recipe.ingredients.indices.sorted { recipe.ingredients[$0].name < recipe.ingredients[$1].name }
+        } else {
+            return Array(recipe.ingredients.indices)
+        }
+    }
     
     var body: some View {
-        ScrollView{
+        ScrollView {
             
-            VStack(alignment: .leading, spacing: 10) {
+            VStack(alignment: .leading, spacing: 20) {
                 
                 EditableFieldView(title: "Prep Time (mins)", value: $recipe.prep_time)
                 EditableFieldView(title: "Cook Time (mins)", value: $recipe.cook_time)
                 EditableFieldView(title: "Serving", value: $recipe.serving)
                 
                 
-                HStack(alignment: .top) {
-                    
-                    VStack(alignment: .leading, spacing: 6) {
-                        Text("Ingredients")
-                    }
-                    .font(.headline)
-
+                // MARK: INGREDIENTS HEADER
+                HStack {
+                    Text("Ingredients")
+                        .font(.title3).bold()
                     
                     Spacer()
                     
                     Toggle("Sort by name", isOn: $sortByName)
-                            .labelsHidden()
-                     }
-                
-
+                    
+                }
                 
                 
+                // MARK: INGREDIENTS LIST
+                VStack(spacing: 12) {
+                    ForEach(sortedIndices, id: \.self) { index in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text("\(recipe.ingredients[index].name) (\(recipe.ingredients[index].unit ?? ""))")
+                                    .font(.subheadline)
+                                
+                                TextField(
+                                    "Quantity",
+                                    value: $recipe.ingredients[index].quantity,
+                                    formatter: NumberFormatter()
+                                )
+                                .keyboardType(.decimalPad)
+                                .textFieldStyle(.roundedBorder)
+                            }
+                            
+                            Spacer()
+                            
+                            
+                        }
+                        .padding(.vertical, 4)
+                    }
+                }
             }
-            .navigationTitle(recipe.name)
-            
             .padding()
-            
         }
+        .navigationTitle(recipe.name)
     }
     
-        
 }
+    
+    //Utilitaires
+    
+
