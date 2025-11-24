e46c4fa48d66f69155bc946a376286ae43a22184
Massil1099 <massilaitchallal2002@gmail.com>
2025-11-24 10:08:21 +0100

    RecipeEditView: Adding the delete Button for the ingredients edit

--- RecipesApp/view/RecipeEditView.swift
+++ RecipesApp/view/RecipeEditView.swift
@@ -15,7 +15,7 @@ struct RecipeEditView: View {
     @Binding var recipe: Recipe
     @State private var sortByName = false
     
-    /// Retourne les indices triés selon le toggle
+    // on retourne les indices triés selon le bouton toggle
     var sortedIndices: [Int] {
         if sortByName {
             return recipe.ingredients.indices.sorted { recipe.ingredients[$0].name < recipe.ingredients[$1].name }
@@ -34,7 +34,6 @@ struct RecipeEditView: View {
                 EditableFieldView(title: "Serving", value: $recipe.serving)
                 
                 
-                // MARK: INGREDIENTS HEADER
                 HStack {
                     Text("Ingredients")
                         .font(.title3).bold()
@@ -46,10 +45,11 @@ struct RecipeEditView: View {
                 }
                 
                 
-                // MARK: INGREDIENTS LIST
+                // la liste des ingredients
                 VStack(spacing: 12) {
                     ForEach(sortedIndices, id: \.self) { index in
                         HStack {
+                            
                             VStack(alignment: .leading) {
                                 Text("\(recipe.ingredients[index].name) (\(recipe.ingredients[index].unit ?? ""))")
                                     .font(.subheadline)
@@ -65,19 +65,31 @@ struct RecipeEditView: View {
                             
                             Spacer()
                             
+                            Button {
+                                deleteIngredient(at:index)
+                            } label: {
+                                Image(systemName: "trash")
+                                    .foregroundColor(.white)
+                                    .padding(10)
+                                    .background(Color.red)
+                                    .clipShape(Circle())
                             
+                            }
+                            .padding(.vertical, 4)
                         }
-                        .padding(.vertical, 4)
                     }
                 }
+                .padding()
             }
-            .padding()
+            .navigationTitle(recipe.name)
         }
-        .navigationTitle(recipe.name)
+        
+        
+        
     }
     
+    private func deleteIngredient(at index: Int) {
+        recipe.ingredients.remove(at: index)
+    }
 }
-    
-    //Utilitaires
-    
 
